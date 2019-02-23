4db6daafa62fa37ee6f2ce5547dda918774a3828
Igor Sudenko <sudenkoweb@gmail.com>
2019-02-24 01:07:54 +0200

    screen: fixed onePageScroll;

--- src/js/Animator.js
+++ src/js/Animator.js
@@ -4,18 +4,18 @@ import PubSub from 'pubsub-js';
 import {TimelineLite} from 'gsap';
 
 PubSub.subscribe('gotoSlide', function(msg, data) {
+  const tl = new TimelineLite();
   const currentSlide = document.querySelector('[data-slide="' + data.from + '"]');
   const newSlide = document.querySelector('[data-slide="' + data.to + '"]');
+  let windowWidth = document.body.getBoundingClientRect().width;
 
-  const tl = new TimelineLite();
-
-  if (!data.stopSlide) {
+  if (!data.stopSlide && windowWidth > 900) {
     if (data.from < data.to) {
       newSlide.classList.add('screen--show');
-      tl.fromTo(newSlide, 0.5, {y: '100%'}, {y: '0%'}, 0);
+      tl.fromTo(newSlide, 0.5, {css: {top: '60%'}}, {css: {top: '0%'}}, 0);
     } else {
       newSlide.classList.add('screen--show');
-      tl.fromTo(currentSlide, 0.5, {y: '0%'}, {y: '100%'}, 0);
+      tl.fromTo(currentSlide, 0.5, {css: {top: '0%'}}, {css: {top: '97%'}}, 0);
     }
   }
 });
